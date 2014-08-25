43dbda0fd74c8a74a5bac1457ec78276ec755dba
Justin SB <justin@fathomdb.com>
2014-08-25 08:28:43 -0700

    Be a well-behaved cache; no debian specific hacks

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ RUN apt-get update && apt-get -y install squid-deb-proxy
 RUN ln -s /cachedata /var/cache/squid-deb-proxy
 VOLUME ["/cachedata"]
 
-EXPOSE 3128 8000
+EXPOSE 8000:3128
 
 ENTRYPOINT ["/start.sh"]
 
--- files/start.sh
+++ files/start.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+LOGFILE=/var/log/squid-deb-proxy/access.log
+touch ${LOGFILE}
+chown proxy:proxy ${LOGFILE}
+tail -f ${LOGFILE} &
+
 /etc/init.d/squid-deb-proxy start
 
 while [ 1 ]
 do
-	sleep 30
-	# TODO: Auto restart proxy? (service squid-deb-proxy status | grep running) || service squid-deb-proxy start
+	sleep 10
+	# Auto restart proxy
+	(ps --pid `cat /var/run/squid-deb-proxy.pid` > /dev/null) || /etc/init.d/squid-deb-proxy start
 done
